903f0c713553170d9d157ba48f8b16cb084eb0ed
tenor1 <tenor@ukr.net>
2024-11-13 00:21:20 +0200

    L113. Loading Data On Startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,7 @@
 const { parse } = require("csv-parse");
+// const parse = require('csv-parse');
 const fs = require("fs");
+const path = require("path");
 
 const habitablePlanets = [];
 
@@ -8,23 +10,41 @@ function isHabitablePlanet(planet) {
     && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
     && planet["koi_prad"] < 1.6;
 }
+/*
+const promise = new Promise((resolve, reject) => {
+  resolve(42);
+});
+promise.then((result) => {
+  
+});
+const result = await promise;
+console.log(result);
+*/
 
-fs.createReadStream("kepler_data.csv")
-  .pipe(parse({ comment: "#", columns: true }))
-  .on("data", (data) => {
-    // console.log(data);
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-    
-  })
-  .on("end", () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet["kepler_name"];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found`);
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', "kepler_data.csv"))
+    .pipe(parse({ comment: "#", columns: true }))
+    .on("data", (data) => {
+      if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+      }      
+    })
+    .on("error", (err) => {
+      console.log(err);
+      reject(err);
+    })
+    .on("end", () => {
+      // console.log(habitablePlanets.map((planet) => {
+      //   return planet["kepler_name"];
+      // }));
+      console.log(`${habitablePlanets.length} habitable planets found`);
+      resolve();
+    });
   });
+}
 
-  module.exports = {
-    planets: habitablePlanets
-  }
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets
+}
