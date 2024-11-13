292bbea19c1ba8f2407b28b9f83bcd22a03ea3fc
tenor1 <tenor@ukr.net>
2024-11-13 18:41:43 +0200

    L132. Testing APIs wih Jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,16 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe('Test POST /launches', () => {
+  test('It should respond with 201 created', () => {
+    const response = 201;
+    expect(response).toBe(201);
+  });
+
+  test('It should catch missing required properties', () => {});
+  test('It should catch invalid dates', () => {});
+});
