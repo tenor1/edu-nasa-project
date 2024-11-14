b3bbb2eb8d8e60fa392772ad1b9649472620f529
tenor1 <tenor@ukr.net>
2024-11-14 20:59:59 +0200

    L193. Loading SpaceX Data In Our API

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
 const http = require("http");
-// const mongoose = require("mongoose");
 const app = require("./app");
 const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +11,8 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData()
+
   server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 }
 
