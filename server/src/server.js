5ec33412f5c8142958eba9b4692fd8668181ffd2
tenor1 <tenor@ukr.net>
2024-11-14 10:26:18 +0200

    L160. Connecting to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = `mongodb+srv://tenor:Tenor%23483@cluster0.cmtku.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0`;
+
 const server = http.createServer(app);
+// mongoose.connection.on("open", () => console.log("MongoDB connection ready!"));
+mongoose.connection.once("open", () => console.log("MongoDB connection ready!"));
+mongoose.connection.on("error", (err) => console.log(err));
 
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 }
