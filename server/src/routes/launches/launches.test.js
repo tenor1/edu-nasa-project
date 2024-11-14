8121d39f4b909dfd71176ef1c2d74b224a99803f
tenor1 <tenor@ukr.net>
2024-11-14 17:38:21 +0200

    L185. Updating Tests For Mongoose 2

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
 const app = require('../../app');
-const { mongoConnect } = require('../../services/mongo');
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+  });
+
+  afterAll(async () => {
+    await mongoDisconnect();
   });
 
   describe('Test GET /launches', () => {
