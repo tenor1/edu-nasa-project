7b9c5a16162dde9dc1df10c9451b5b625d0917cc
tenor1 <tenor@ukr.net>
2024-11-14 17:25:19 +0200

    L184. Updating Tests For Mongoose 1

---
+++ server/src/services/mongo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = `mongodb+srv://tenor:Tenor%23483@cluster0.cmtku.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.connection.once("open", () => console.log("MongoDB connection ready!"));
+mongoose.connection.on("error", (err) => console.log(err));
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+module.exports = {
+  mongoConnect
+}
