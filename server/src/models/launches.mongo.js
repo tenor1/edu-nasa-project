1817f594ab3821123c688700f3895dac316d6b2a
tenor1 <tenor@ukr.net>
2024-11-14 11:28:01 +0200

    L166. Creating Models from Schemas

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -36,3 +36,6 @@ const launchesSchema = new mongoose.Schema({
     default: true,
   },
 });
+
+// connects launchesSchema to "launches" collection
+module.exports = mongoose.model('Launch', launchesSchema);
