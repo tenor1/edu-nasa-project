b6c23666a25eb2177bab8d4594055aa8f698b70a
tenor1 <tenor@ukr.net>
2024-11-14 19:05:21 +0200

    L189. Versioning Node APIs. L190. Updating Our API Tests

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const api = require("./routes/api");
 
 const app = express();
 
@@ -14,8 +12,10 @@ app.use(cors({
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+
+app.use('/v1', api);
+// app.use('/v2', v2Router);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
