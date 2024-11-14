ad5379244a8db8e4ddec51508f3131cb6fc3fc61
tenor1 <tenor@ukr.net>
2024-11-14 13:18:22 +0200

    L174. Excluding fields from the response

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -33,7 +33,7 @@ function loadPlanetsData() {
 
 async function getAllPlanets() {
   // return habitablePlanets;
-  return await planets.find();
+  return await planets.find({}, {'_id':0, '__v':0});
 }
 
 async function savePlanet(planet) {
