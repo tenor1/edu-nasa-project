8e5965cfbd0f5c81c487398c1b42750818b82a57
tenor1 <tenor@ukr.net>
2024-11-15 09:25:16 +0200

    L202. Managing Secrets with Dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = `mongodb+srv://tenor:Tenor%23483@cluster0.cmtku.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0`;
+const MONGO_URL = process.env.MONGO_URL;
 mongoose.connection.once("open", () => console.log("MongoDB connection ready!"));
 mongoose.connection.on("error", (err) => console.log(err));
 
--- server/src/services/query.js
+++ server/src/services/query.js
@@ -1,5 +1,5 @@
 const DEFAULT_PAGE = 1;
-const DEFAULT_PAGE_LIMIT = 1; // 0;
+const DEFAULT_PAGE_LIMIT = 100; // 0;
 
 function getPagination(query) {
   const page = Math.abs(query.page) || DEFAULT_PAGE;
