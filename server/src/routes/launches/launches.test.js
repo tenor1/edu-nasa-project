4cf2457fc37a9f8902c90706440b403770262dae
tenor1 <tenor@ukr.net>
2024-11-18 15:27:40 +0200

    Pass client tests with no test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
-const { loadLaunchData } = require('../../models/launches.model');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
-    await loadLaunchData();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
